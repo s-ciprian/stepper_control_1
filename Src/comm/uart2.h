fcd6ce4941127bb48725b5bc21487775a6eaadf6
ciprian salageanu <cipriansalageanu@yahoo.com>
2015-11-22 22:54:52 +0200

    Added uart modules

---
+++ Src/comm/uart2.h
@@ -0,0 +1,10 @@
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __UART2_H
+#define __UART2_H
+
+
+
+#endif /* __UART2_H */
+
+/* Includes ------------------------------------------------------------------*/
