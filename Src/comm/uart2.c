249078c5fdd66248b8ae20aaf534d0927534de60
ciprian <ciprian.salageanu@aem.ro>
2015-12-03 16:19:34 +0200

    Added comments on uart2 module. Some variables/defines name changed.

--- Src/comm/uart2.c
+++ Src/comm/uart2.c
@@ -1,109 +1,133 @@
-
+/*! \file
+ * \details This file  implements functions to control UART2 peripheral.
+ */
+ 
+/* Includes ------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/ 
 #include "main.h"
 #include "uart2.h"
 
+/* Private typedef -----------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
-//---- Cip: UART. Check
-/* Definition for USARTx clock resources */
-#define USARTx                           USART2
+/* Private define ------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/* Definition for USART 2 Pins */    
+#define USART2_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
+#define USART2_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
 
-#define USARTx_CLK_ENABLE()              __HAL_RCC_USART2_CLK_ENABLE();
-#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
-#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
-
-#define USARTx_FORCE_RESET()             __HAL_RCC_USART2_FORCE_RESET()
-#define USARTx_RELEASE_RESET()           __HAL_RCC_USART2_RELEASE_RESET()
-
-/* Definition for USARTx Pins */
-#define USARTx_TX_PIN                    GPIO_PIN_2
-#define USARTx_TX_GPIO_PORT              GPIOA
-#define USARTx_TX_AF                     GPIO_AF7_USART2
-#define USARTx_RX_PIN                    GPIO_PIN_3
-#define USARTx_RX_GPIO_PORT              GPIOA
-#define USARTx_RX_AF                     GPIO_AF7_USART2
-//---- Cip: UART
+#define USART2_TX_PIN                    GPIO_PIN_2
+#define USART2_TX_GPIO_PORT              GPIOA
+#define USART2_TX_AF                     GPIO_AF7_USART2
+#define USART2_RX_PIN                    GPIO_PIN_3
+#define USART2_RX_GPIO_PORT              GPIOA
+#define USART2_RX_AF                     GPIO_AF7_USART2
 
 
+/* Private macro -------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
+/* Private variables ---------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/** \brief UART handler declaration */
+ UART_HandleTypeDef hUart_2;
 
+/* Private function prototypes -----------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
-/* UART handler declaration */
- UART_HandleTypeDef UartHandle;
+/* Exported functions --------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
-
-
+/**
+  * \brief Configure UART 2 peripheral
+  * \param None
+  * \retval None
+  *
+  * TODO: Add an API to retrive the address of hUart_2, then pass
+  *       this address to this function. If changes are nedded to
+  *       default configuration this can be done.
+  */
 void uart2_Init(void)
 {
-	//######################################
-	//---- Cip: UART test init pins
 	GPIO_InitTypeDef  GPIO_InitStruct;
 
 	/*##-1- Enable peripherals and GPIO Clocks #################################*/
 	/* Enable GPIO TX/RX clock */
-	USARTx_TX_GPIO_CLK_ENABLE();
-	USARTx_RX_GPIO_CLK_ENABLE();
+	USART2_TX_GPIO_CLK_ENABLE();
+	USART2_RX_GPIO_CLK_ENABLE();
 
-	/* Enable USARTx clock */
-	USARTx_CLK_ENABLE();
+	/* Enable USART 2 clock */
+	__HAL_RCC_USART2_CLK_ENABLE();
 
 	/*##-2- Configure peripheral GPIO ##########################################*/
 	/* UART TX GPIO pin configuration  */
-	GPIO_InitStruct.Pin       = USARTx_TX_PIN;
+	GPIO_InitStruct.Pin       = USART2_TX_PIN;
 	GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
 	GPIO_InitStruct.Pull      = GPIO_PULLUP;
 	GPIO_InitStruct.Speed     = GPIO_SPEED_FAST;
-	GPIO_InitStruct.Alternate = USARTx_TX_AF;
+	GPIO_InitStruct.Alternate = USART2_TX_AF;
 
-	HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+	HAL_GPIO_Init(USART2_TX_GPIO_PORT, &GPIO_InitStruct);
 
 	/* UART RX GPIO pin configuration  */
-	GPIO_InitStruct.Pin = USARTx_RX_PIN;
-	GPIO_InitStruct.Alternate = USARTx_RX_AF;
+	GPIO_InitStruct.Pin = USART2_RX_PIN;
+	GPIO_InitStruct.Alternate = USART2_RX_AF;
 
-	HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
-	//######################################
+	HAL_GPIO_Init(USART2_RX_GPIO_PORT, &GPIO_InitStruct);
 
-	//---- Cip: UART. Check
 	/* UART2 configured as follow:
 	  - Word Length = 9 Bits
 	  - Stop Bit = One Stop bit
 	  - Parity = NO parity
 	  - BaudRate = 9600 baud
 	  - Hardware flow control disabled (RTS and CTS signals) */
-	UartHandle.Instance          = USARTx;
+	hUart_2.Instance          = USART2;
 
-	UartHandle.Init.BaudRate     = 9600;
-	UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
-	UartHandle.Init.StopBits     = UART_STOPBITS_1;
-	UartHandle.Init.Parity       = UART_PARITY_NONE;
-	UartHandle.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
-	UartHandle.Init.Mode         = UART_MODE_TX_RX;
-	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+	hUart_2.Init.BaudRate     = 9600;
+	hUart_2.Init.WordLength   = UART_WORDLENGTH_8B;
+	hUart_2.Init.StopBits     = UART_STOPBITS_1;
+	hUart_2.Init.Parity       = UART_PARITY_NONE;
+	hUart_2.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
+	hUart_2.Init.Mode         = UART_MODE_TX_RX;
+	hUart_2.Init.OverSampling = UART_OVERSAMPLING_16;
 
-	if(HAL_UART_Init(&UartHandle) != HAL_OK)
+	if(HAL_UART_Init(&hUart_2) != HAL_OK)
 	{
 		/* Initialization Error */
 		Error_Handler(1002);
 	}
-	//---- Cip: UART. Check
 }
 
-
+/**
+  * \brief Disable UART 2 peripheral
+  * \param None
+  * \retval None
+  *
+  *
+  */
 void uart2_DeInit(void)
 {
 	/*##-1- Reset peripherals ##################################################*/
-	USARTx_FORCE_RESET();
-	USARTx_RELEASE_RESET();
+	__HAL_RCC_USART2_FORCE_RESET();
+	__HAL_RCC_USART2_RELEASE_RESET();
 
 	/*##-2- Disable peripherals and GPIO Clocks #################################*/
 	/* Configure UART Tx as alternate function  */
-	HAL_GPIO_DeInit(USARTx_TX_GPIO_PORT, USARTx_TX_PIN);
+	HAL_GPIO_DeInit(USART2_TX_GPIO_PORT, USART2_TX_PIN);
 	/* Configure UART Rx as alternate function  */
-	HAL_GPIO_DeInit(USARTx_RX_GPIO_PORT, USARTx_RX_PIN);
+	HAL_GPIO_DeInit(USART2_RX_GPIO_PORT, USART2_RX_PIN);
 }
 
+/**
+  * \brief UART 2 transmit function
+  * \param pointer to buffer to be sent, size of the buffer
+  * \retval None
+  */
 void uart2_Transmit(uint8_t *pData, uint16_t Size)
 {
-	HAL_UART_Transmit(&UartHandle, pData, Size, 0xFFFF);
+	HAL_UART_Transmit(&hUart_2, pData, Size, 0xFFFF);
 }
+
+/* Private functions ---------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 
