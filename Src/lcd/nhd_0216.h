c7477cf32716576c5fd10bf3b87bd002dc96eb90
Ciprian Salageanu <cipriansalageanu@gmail.com>
2016-11-27 20:35:38 +0200

    Start integration of LCD. Added files and high level structure. TODO: Add LCD initilaization.

---
+++ Src/lcd/nhd_0216.h
@@ -0,0 +1,17 @@
+#ifndef __NHD_0216_H__
+#define __NHD_0216_H__
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int16_t lcd_Init(void);
+void lcd_Put_Act_Pos(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __NHD_0216_H__
